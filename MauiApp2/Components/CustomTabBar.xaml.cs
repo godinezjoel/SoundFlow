24fbe9b46c8ac840b29226fefc7a16d2a3aa1936
jgo151612 <jgo151612@stud.gibb.ch>
2025-12-11 13:06:58 +0100

    NavBar

---
+++ MauiApp2/Components/CustomTabBar.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiApp2.Components;
+
+public partial class CustomTabBar : ContentView
+{
+    public CustomTabBar()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnHomeClicked(object? sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//HomePage");
+    }
+
+    private async void OnLikedClicked(object? sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//LikedSongsPage");
+    }
+
+    private async void OnStatsClicked(object? sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//StatsPage");
+    }
+
+    private async void OnAccountClicked(object? sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//AccountPage");
+    }
+}
